b7cf8899486e4bd5432daea4d5da6b05bf8c63fd
Lucas Gabriel Dutra de Souza <lucasgdutra@gmail.com>
2023-10-18 16:43:11 -0300

    configurações vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,24 @@
 import { defineConfig } from 'astro/config';
 
+
+import vercelStatic from '@astrojs/vercel/static';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop'
+    }
+  },
+  adapter: vercelStatic({
+    
+    imagesConfig: {
+      sizes: [200, 320, 450, 640, 1280],
+      "formats": ["image/avif", "image/webp"],
+      domains:[],
+     
+    },
+    imageService: true,
+  })
+});
